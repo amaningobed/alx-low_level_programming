2fe48f166f8d221e870f7d4d4b0b5daddabf6a4d
amaningobed <amaningobed@DESKTOP-KH836TI.localdomain>
2023-07-19 16:54:58 -0700

    at long last

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /*
-  * print_alphabet_x10 - Print lowercase alphabet 10 times
- */
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
+ * */
 
 void print_alphabet_x10(void)
 {
@@ -12,7 +13,6 @@ for (num = 0; num <= 9; num++)
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
-}
 _putchar('\n');
 }
 }
